f4896ee643b7073f2b4f4c9495eab9a04986307c
Maximilian <scientificresearch309@gmail.com>
2025-10-21 23:31:37 +0200

    Route for Login added to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-// import './App.css';
 import './styles/App.scss';
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageDashboard } from './pages/PageDashboard';
 import { PageJobs } from './pages/PageJobs';
 import { PageSkills } from './pages/PageSkills';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { PageLogin } from './pages/PageLogin';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <h1>Get a Job</h1>
-
       <nav>
-        <NavLink to="dashboard">Dashboard</NavLink>
-        <NavLink to="jobs">Jobs</NavLink>
-        <NavLink to="skills">Skills</NavLink>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/jobs">Jobs</NavLink>
+        <NavLink to="/skills">Skills</NavLink>
+        <NavLink to="/login">Login</NavLink>
       </nav>
 
       <Routes>
-        <Route path="dashboard" element={<PageDashboard />}></Route>
-        <Route path="jobs" element={<PageJobs />}></Route>
-        <Route path="skills" element={<PageSkills />}></Route>
-        <Route path="/" element={<Navigate to="dashboard" />}></Route>
+        <Route path="/dashboard" element={<PageDashboard />} />
+        <Route path="/jobs" element={<PageJobs />} />
+        <Route path="/skills" element={<PageSkills />} />
+        <Route path="/login" element={<PageLogin />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </div>
   );
